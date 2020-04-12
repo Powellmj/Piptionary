fd9aa5589cbc413e5994a1773bd7c604ad9ff66a
MichaelPowell <gieringmj@gmail.com>
2020-04-12 00:04:03 -0400

    Set up backend registration and login

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const express = require("express");
 const app = express();
 const users = require("./routes/api/users");
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
-
-app.get("/", (req, res) => res.send("Hello World"));
+app.use(passport.initialize());
+require('./config/passport')(passport);
 app.use("/api/users", users);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
