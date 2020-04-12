09b4eb625947286c2e607a205164ac430ccc8b01
MichaelPowell <gieringmj@gmail.com>
2020-04-11 23:50:36 -0400

    Add validations for user

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
+const users = require("./routes/api/users");
+const bodyParser = require('body-parser');
+
 
 app.get("/", (req, res) => res.send("Hello World"));
+app.use("/api/users", users);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 const db = require('./config/keys').mongoURI;
