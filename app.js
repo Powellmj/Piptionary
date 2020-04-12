f85249ef4d5780ed51e9de5b1ca3514a7b1b0f2c
MichaelPowell <gieringmj@gmail.com>
2020-04-12 02:17:00 -0400

    Setting up for heroku

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const app = express();
 const users = require("./routes/api/users");
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 app.use(passport.initialize());
 app.use(bodyParser.urlencoded({ extended: false }));
