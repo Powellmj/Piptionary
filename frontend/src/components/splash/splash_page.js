3487cf1b714d91be56e43060356fe964498166dd
MichaelPowell <gieringmj@gmail.com>
2020-04-20 13:28:19 -0400

    Integrate bootstrap for splash

--- frontend/src/components/splash/splash_page.js
+++ frontend/src/components/splash/splash_page.js
@@ -7,14 +7,14 @@ class MainPage extends React.Component {
   render() {
     return (
       <div className="splash-container">
-        <div className="splash-left-container">
+        <div className="container-fluid splash-left-container">
           <div>
             <div>
               <div className="splash-image"></div>
             </div>
           </div>
         </div>
-        <div className="splash-right-container">
+        <div className="container-sm splash-right-container">
           <SessionFormContainer />
         </div>
       </div>
